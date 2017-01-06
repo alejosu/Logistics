7afece07bf9394010415abd568b4b61b783daf21
alejosu - SoftMAS <alejosu@gmail.com>
2017-01-05 20:47:12 -0500

    Interfaz de pag viajes
    Collapse

--- resources/views/pages/viajes.blade.php
+++ resources/views/pages/viajes.blade.php
@@ -6,45 +6,33 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Viajes de solicitud {{ $solicitud->id }}</div>
+                <div class="panel-heading"><a href="crearViaje" class="btn btn-xs btn-primary pull-right">Crear viaje</a></div>                
                 <div class="panel-body">
-                    <a href="crearViaje" class="btn btn-xs btn-primary pull-right">Crear viaje</a>
-                    <table class="table table-condensed table-hover">
-                        <thead>
-                            <th>Id</th>
-                            <th>Solicitud</th>
-                            <th>Cantidad</th>
-                            <th>Fecha entrega</th>
-                            <th>Fecha salida</th>
-                            <th>Manifiesto</th>
-                            <th>Vehiculo</th>
-                            <th>Origen</th>
-                            <th>Destino</th>
-                            <th>Medida</th>
-                            <th>Destinatario</th>
-                            <th>Estado</th>
-                            <th>Creación</th>
-                            
-                        </thead>
-                        <tbody>
-                            @foreach ($viajes as $viaje)
-                                <tr>
-                                    <td>{{ $viaje->id }}</td>
-                                    <td>{{ $solicitud->id }}</td>
-                                    <td>{{ $viaje->cantidad }}</td>
-                                    <td>{{ $viaje->fecha_salida }}</td>
-                                    <td>{{ $viaje->fecha_entrega }}</td>
-                                    <td>{{ $viaje->manifiesto }}</td>
-                                    <td>{{ $viaje->placa }}</td>
-                                    <td>{{ $viaje->origen }} - {{ $viaje->dir_origen }}</td>
-                                    <td>{{ $viaje->destino }} - {{ $viaje->dir_destino }}</td>
-                                    <td>{{ $viaje->medida }}</td>
-                                    <td>{{ $viaje->destinatario }}</td>
-                                    <td>{{ $viaje->estado }}</td>
-                                    <td>{{ $viaje->created_at }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        
+                        @foreach ($viajes as $viaje)  
+                          <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="heading {{ $viaje->id }}">
+                              <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#{{ $viaje->id }}" aria-expanded="true" aria-controls="{{ $viaje->id }}">
+                                  Viaje: {{ $viaje->id }} - {{ $viaje->origen}} - {{ $viaje->dir_origen }} - {{ $viaje->destino}} - {{ $viaje->dir_destino }} 
+                                </a>
+                              </h4>
+                            </div>
+                            <div id="{{ $viaje->id }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading{{ $viaje->id }}">
+                              <div class="panel-body">
+                                <strong>Estado: </strong> {{ $viaje->estado }} <br>
+                                <strong>Cantidad: </strong>{{ $viaje->cantidad }} {{ $viaje->medida }} <br>
+                                <strong>Placa: </strong> {{ $viaje->placa }} <br>
+                                <strong>Fecha Salida: </strong> {{ $viaje->fecha_salida }} <br>
+                                <strong>Fecha Entrega: </strong> {{ $viaje->fecha_entrega}} <br>
+                                <strong>Destinatario: </strong> {{ $viaje->destinatario }}
+                              </div>
+                            </div>
+                          </div>
+                          @endforeach
+
+                    </div>
                 </div>
             </div>
         </div>
